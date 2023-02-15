76040dd36cbfcd38a6444277655304a6718cbfc5
Robert Benak <robertbenak@MacBook-Air-von-Robert.local>
2023-02-15 15:55:56 +0100

    changed output

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ async function run() {
     core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
     await wait(parseInt(ms));
     core.info((new Date()).toTimeString());
-
+    
+    core.debug("SignPath Test");
     core.setOutput('time', new Date().toTimeString());
   } catch (error) {
     core.setFailed(error.message);
